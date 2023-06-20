1d8580a2b2917271852145a04c5e8ecdebbbb6a4
SofPan <84824782+SofPan@users.noreply.github.com>
2023-06-20 14:14:24 -0400

    Create password change handler function and set to onChange attribute for password field input

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,35 +1,49 @@
 import { useState } from "react";
 
 const Form = () => {
-    /*
+	/*
         Initialize state for form inputs
-    */ 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+    */
+	const [email, setEmail] = useState("");
+	const [password, setPassword] = useState("");
 
-
-    const emailChangeHandler = (e) => {
-        /*
+	const emailChangeHandler = (e) => {
+		/*
             Set email to user input
-        */ 
-        setEmail(e.target.value);
-    }
+        */
+		setEmail(e.target.value);
+	};
 
-    return(
-        <form>
-            <div>
-                <label>Email</label>
-                <input type="email" value={email} onChange={emailChangeHandler} />
-            </div>
-            <div>
-                <label>Password</label>
-                <input type="password" value={password} />
-            </div>
-            <div>
-                <button>Login</button>
-            </div>
-        </form>
-    )
-}
+	const passwordChangeHandler = (e) => {
+		/*
+            Set password to user input
+        */
+		setPassword(e.target.value);
+	};
+
+	return (
+		<form>
+			<div>
+				<label>Email</label>
+				<input
+					type='email'
+					value={email}
+					onChange={emailChangeHandler}
+				/>
+			</div>
+			<div>
+				<label>Password</label>
+				<input
+					type='password'
+					value={password}
+					onChange={passwordChangeHandler}
+				/>
+			</div>
+			<div>
+				<button>Login</button>
+			</div>
+		</form>
+	);
+};
 
 export default Form;
