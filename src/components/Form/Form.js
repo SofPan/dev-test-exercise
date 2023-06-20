3b8b87dcecdc0ba401a83076de82a3d4384d59c9
SofPan <84824782+SofPan@users.noreply.github.com>
2023-06-20 14:09:49 -0400

    Create form email and password inputs, as well as submit button

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -2,7 +2,17 @@
 const Form = () => {
     return(
         <form>
-
+            <div>
+                <label>Email</label>
+                <input type="email" value="email@email.com" />
+            </div>
+            <div>
+                <label>Password</label>
+                <input type="password" value="" placeholder="password" />
+            </div>
+            <div>
+                <button>Login</button>
+            </div>
         </form>
     )
 }
